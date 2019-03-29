046203753d482cd712b971ae219840e5c7ab0abd
Marvin <mardancely@gmail.com>
2019-03-29 12:13:57 -0500

    Bases de node Yargs

--- node/03-bases-node/01-app.js
+++ node/03-bases-node/01-app.js
@@ -1,8 +1,23 @@
-const { crearArchivo } = require('./multiplicar/multiplicar')
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la abla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b',
+        },
+        limite: {
+            alias: 'l',
+            default: 10,
+        }
+    })
+    .argv;
+const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let parametro = process.argv[2];
-let base = parametro.split('=')[1];
+console.log('Limite', argv.limite);
 
-crearArchivo(base)
-    .then((archivo) => console.log(`Archivo creado ${archivo}`))
-    .catch((e) => console.log(e));
+
+// let parametro = process.argv[2];
+// let base = parametro.split('=')[1];
+
+// crearArchivo(base)
+//     .then((archivo) => console.log(`Archivo creado ${archivo}`))
+//     .catch((e) => console.log(e));
